dc8ce3e480318bc01d9287c491a4f758971da968
Jeremiah Crowell <jeremiah.c2001@gmail.com>
2025-12-01 22:22:44 -0600

    25.1.2 haskell

---
+++ 2025/day01/solver2.hs
@@ -0,0 +1,46 @@
+import Control.Exception (IOException, catch)
+import System.CPUTime (getCPUTime)
+import System.Environment (getArgs)
+import Text.Printf (printf)
+
+directionToInt :: String -> Int
+directionToInt (x : xs)
+  | x == 'R' = read xs
+  | x == 'L' = -(read xs)
+
+-- takes start num and list and counts how many times the total is 0
+-- including in passing
+countZeros :: Int -> [Int] -> Int
+countZeros curr [] = 0
+countZeros curr (x : xs) =
+  let newVal = curr + x
+      turnedLeftPastZero = if (newVal <= 0) && (curr > 0) then 1 else 0
+      nPassedZero = abs newVal `div` 100
+   in turnedLeftPastZero + nPassedZero + countZeros (newVal `mod` 100) xs
+
+main :: IO ()
+main =
+  do
+    args <- getArgs
+    case args of
+      [] -> putStrLn "Usage: runhaskell solver_template.hs <input_filename>"
+      (inputFilename : _) -> do
+        startTime <- getCPUTime
+
+        -- Read input file line by line
+        content <- readFile inputFilename
+        let linesOfInput = lines content
+
+        let signedDirections = map directionToInt linesOfInput
+
+        let solution = countZeros 50 signedDirections
+
+        endTime <- getCPUTime
+        let elapsedSeconds = fromIntegral (endTime - startTime) / 1e12 :: Double
+
+        print solution
+        putStrLn $ printf "%.6fs" elapsedSeconds
+    `catch` handleIOError
+  where
+    handleIOError :: IOException -> IO ()
+    handleIOError e = putStrLn $ "Error reading file: " ++ show e
